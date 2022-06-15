228d9f74522513e5d48d60f819e5a90d9275d07d
Eng.Basel <besooking22@gmail.com>
2022-06-15 19:25:39 +0300

    Initial commit

---
+++ lib/app/app.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mvvm/presentation/resourcese/theme_manager.dart';
+
+class MyApp extends StatefulWidget {
+
+  int appState = 0;
+  //named constructor
+  MyApp._internal();
+
+  static final MyApp _instance = MyApp._internal(); //singleton or single instance
+
+  factory MyApp() => _instance;
+
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: getApplicationTheme(),);
+  }
+}
+
+// class test extends StatelessWidget {
+//   const test({Key? key}) : super(key: key);
+// void updateAppState (){
+//  MyApp().appState=11;
+// }
+// void getAppState(){
+//   print( MyApp().appState);//11
+// }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
+// class test2 extends StatelessWidget {
+//   const test2({Key? key}) : super(key: key);
+//
+// void getAppState(){
+//   print( MyApp().appState);//11
+// }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
